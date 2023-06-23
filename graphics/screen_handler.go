2b25cc6ceb0743c6a5454c08adfd7b3a558cb7b7
Moritz Kappels <kappels@internet-sicherheit.de>
2023-06-23 16:49:34 +0200

    add screen_handler for simplifying main file

---
+++ graphics/screen_handler.go
@@ -0,0 +1,13 @@
+package graphics
+
+type calcScreen struct {
+	xScreenMin int
+	xScreenMax int
+	yScreenMin int
+	yScreenMax int
+}
+
+func isOnScreen(xpos float64, ypos float64, width int, height int) bool {
+
+	return false
+}
